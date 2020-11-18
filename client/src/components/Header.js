9948c2a6d58c08ea30e911be77d3dd0b10a857e8
HuhnDaniel <danielphuhn@gmail.com>
2020-11-17 18:45:44 -0600

    working menu links

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,30 +1,34 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
-function Header() {
+function Header({ textColor }) {
     const [menuStatus, setMenuStatus] = useState(false)
 
     function menuToggle(e) {
         e.preventDefault();
 
-        console.log(menuStatus);
+        console.log(e.target.tagName);
         if (menuStatus) {
             setMenuStatus(false);
-        } else {
+        } else if (e.target.tagName === 'BUTTON') {
             setMenuStatus(true);
         }
     }
 
     return (
-        <header className="flex flex-col m-4 text-white items-center">
-            <h1 className="text-5xl">CORNERSTONE</h1>
-            <h2 className="text-2.5xl">ART &amp; CRAFT COLLECTIVE</h2>
-            <button className="text-2.5xl" onClick={menuToggle} data-toggle="hidden">☰</button>
+        <header onClick={menuToggle} className={`flex flex-col m-4 text-2.5xl text-${textColor} items-center`}>
+			<Link to={"/"}>
+            	<h1 className="text-5xl">CORNERSTONE</h1>
+            	<h2>ART &amp; CRAFT COLLECTIVE</h2>
+			</Link>
+            <button>☰</button>
             {
                 menuStatus ? (
-                    <div className="flex flex-col text-2.5xl items-center">
-                        <h1>People</h1>
-                        <h1>Projects</h1>
-                        <h1>Fields</h1>
+                    <div className="flex flex-col items-center">
+						<Link to={"/"}>Home</Link>
+                        <Link to={"/explore/"}>People</Link>
+                        <Link to={"/explore/"}>Projects</Link>
+                        <Link to={"/explore/"}>Fields</Link>
                         <h1>Contact</h1>
                     </div>
                 ) : (
