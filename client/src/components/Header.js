c66c42cff1e52c1dffc0c36ab0bf791a605dcc5f
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-11-22 12:41:08 -0600

    header and footer styled for desktop

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,15 +3,15 @@ import { Link } from "react-router-dom";
 
 function Header({ menuStatus }) {
     return (
-        <header className={`flex flex-col m-4 text-2.5xl items-center`}>
-			<Link to={"/"}>
+        <header className={"flex flex-col md:flex-row my-4 md:mx-8 text-2.5xl md:text-2xl lg:text-2.5xl items-center"}>
+			<Link to={"/"} className="flex-auto">
             	<h1 className="text-5xl">CORNERSTONE</h1>
-            	<h2 className="-mt-4">ART &amp; CRAFT COLLECTIVE</h2>
+            	<h2 className="text-2.5xl -mt-4">ART &amp; CRAFT COLLECTIVE</h2>
 			</Link>
-            <button>☰</button>
             {
                 menuStatus ? (
-                    <div className="flex flex-col items-center">
+                    <div className="md:hidden flex flex-col items-center">
+                        <button>⨯</button>
 						<Link to={"/"}>Home</Link>
                         <Link to={"/people/"}>People</Link>
                         <Link to={"/projects/"}>Projects</Link>
@@ -19,9 +19,16 @@ function Header({ menuStatus }) {
                         <Link to={"/contact/"}>Contact</Link>
                     </div>
                 ) : (
-                    null
+                    <button className="md:hidden">☰</button>
                 )
             }
+            <nav className="hidden md:grid grid-cols-5 flex-auto">
+				<Link to={"/"}>Home</Link>
+                <Link to={"/people/"}>People</Link>
+                <Link to={"/projects/"}>Projects</Link>
+                <Link to={"/fields/"}>Fields</Link>
+                <Link to={"/contact/"}>Contact</Link>
+            </nav>
         </header>
     );
 }
