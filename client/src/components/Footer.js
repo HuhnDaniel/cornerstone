d152cd1480c529be3b3ea54c9b384590ec3075e6
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-11-22 12:41:08 -0600

    header and footer styled for desktop

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 function Footer() {
 	return (
 		<footer className="absolute min-w-full bottom-0 text-xl my-2">
-			<div className="grid grid-cols-5">
-				<Link to={"/"} className="text-center">Home</Link>
-                <Link to={"/people/"} className="text-center">People</Link>
-                <Link to={"/projects/"} className="text-center">Projects</Link>
-                <Link to={"/fields/"} className="text-center">Fields</Link>
-                <Link to={"/contact/"} className="text-center">Contact</Link>
+            <div className="flex flex-col md:flex-row">
+                <div className="flex-1 grid grid-cols-5">
+                    <Link to={"/"} className="text-center">Home</Link>
+                    <Link to={"/people/"} className="text-center">People</Link>
+                    <Link to={"/projects/"} className="text-center">Projects</Link>
+                    <Link to={"/fields/"} className="text-center">Fields</Link>
+                    <Link to={"/contact/"} className="text-center">Contact</Link>
+                </div>
+                <h2 className="flex-1 text-center">&#169; 2020 Cornerstone A&amp;C LLC</h2>
             </div>
-			<h2 className="text-center">&#169; 2020 Cornerstone A&amp;C LLC</h2>
 		</footer>
 	);
 }
