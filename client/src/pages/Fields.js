7849ad6ccd450bd212e8d22d34b3bed458f24bc1
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-11-24 16:24:41 -0600

    requested header/footer changes, homepage now has About Us replacing Home link

--- client/src/pages/Fields.js
+++ client/src/pages/Fields.js
@@ -15,13 +15,6 @@ function Fields({ menuStatus, menuToggle }) {
                 {
                     fields.map((field, i) => {
                         return (
-                            // <article className="rounded-md cursor-pointer mx-auto my-4 p-4 h-80 w-80 hover:shadow-md transition transition-transform duration-200 transform hover:scale-105" key={i}>
-                            //     <h3 className="text-xl pb-4">{field.field}</h3>
-
-                            //     <figure className="flex items-center mx-auto align-middle h-48 w-48">
-                            //         <img className="w-48" src={field.image} alt={`${field.field}`} />
-                            //     </figure>
-                            // </article>
                             <article className={`bg-${field.image} bg-cover rounded-md text-white cursor-pointer mx-auto my-4 h-72 w-72 hover:shadow-md transition transition-transform duration-200 transform hover:scale-105`} key={i}>
                                 <div className="bg-black p-4 rounded-t bg-opacity-30">
                                     <h3 className="text-2xl">{field.field}</h3>
