f976b23bfa2f7b4baba2471e35e7d476a6238cfc
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-11-29 00:42:40 -0600

    add mySQL and Sequelize functionality, link with project

---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+const Router = require('express').Router;
+// const db = require('../models');
+const apiRoutes = Router();
+
+module.exports = apiRoutes;
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 const expressSession = require('express-session');
+const db = require('./models');
+const routes = require('./routes');
 
 app.use(expressSession({ secret: process.env.sessionSecret, resave: false, saveUninitialized: false }));
 
@@ -12,12 +14,21 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('/client/build'));
-}
+};
+
+app.use(routes);
 
 app.get('*', (req, res) => {
     res.json({});
 });
 
-app.listen(PORT, () => {
-    console.log(`Server on port ${PORT}`);
+const syncOptions = { force: false };
+if (process.env.NODE_ENV === 'test') {
+    syncOptions.force = true;
+};
+
+db.sequelize.sync(syncOptions).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server on port ${PORT}`);
+    });
 });
