51f6d38044ec552a75eeb1dd9d9706eafb96b7d5
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2020-12-16 18:44:14 -0600

    about us visibility toggle

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
-function Header({ menuStatus, isHome }) {
+function Header({ menuStatus, aboutUsToggle, isHome }) {
     return (
         <header className={"flex flex-col md:flex-row my-4 md:mx-8 text-2.5xl md:text-2xl lg:text-2.5xl items-center"}>
 			<Link to={"/"} className="flex-1">
@@ -13,7 +13,7 @@ function Header({ menuStatus, isHome }) {
                     <nav className="md:hidden flex flex-col items-center">
                         <button>⨯</button>
                         {
-                            isHome ? ( <Link to={"/"}>Home</Link> ) : ( <h1>About Us</h1> )
+                            isHome ? ( <button onClick={aboutUsToggle}>About Us</button> ) : ( <Link to={"/"}>Home</Link> )
                         }
                         <Link to={"/partners/"}>Partners</Link>
                         <Link to={"/disciplines/"}>Disciplines</Link>
@@ -24,7 +24,7 @@ function Header({ menuStatus, isHome }) {
             }
             <nav className="hidden md:grid grid-cols-3 flex-1 text-center">
                 {
-                    isHome ? ( <h1>About Us</h1> ) : ( <Link to={"/"}>Home</Link> )
+                    isHome ? ( <button onClick={aboutUsToggle}>About Us</button> ) : ( <Link to={"/"}>Home</Link> )
                 }
                 <Link to={"/partners/"}>Partners</Link>
                 <Link to={"/disciplines/"}>Disciplines</Link>
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,18 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 function Homepage({ menuStatus, menuToggle }) {
+    const [aboutUsStatus, setAboutUsStatus] = useState(false);
+
+    function aboutUsToggle(e) {
+        setAboutUsStatus(true);
+    };
+
 	return (
         <main onClick={menuToggle} className="absolute top-0 left-0 bg-homepage bg-fixed min-h-full min-w-full bg-cover text-white">
-            <Header menuStatus={menuStatus} isHome={true} />
+            <Header menuStatus={menuStatus} aboutUsToggle={aboutUsToggle} isHome={true} />
             
-			<article className="flex flex-col space-y-4 text-lg m-8 md:m-16 items-center justify-items-center bg-black py-4 rounded-lg bg-opacity-30 mb-24">
-				<p className="w-4/5 indent">The name is the brand - we are design and technical professionals, collaborating together to bring art to our disciplines, and craftsmanship to our projects. Our skills range from architecture to graphic arts, and from creative writing to web development. Commitment to art and craft is the cornerstone of our work - building a reliable foundation for the services we provide.</p>
-				<p className="w-4/5 indent">We believe all work has intrinsic value. Some of the work on this web site is for business purposes, and some is a form of creative expression. All is intended as contribution to the common good as we complement each other's skills and strengths.</p>
-				<p className="w-4/5 indent">If you have a project you would like us to collaborate with you on as a client - or if you want to bring your skills to the group - please reach out to one of our maker-partners. You can search for our offerings and project examples either by "Design Disciplines" or by "Collaboration Partners".</p>
-			</article>
+            {
+                aboutUsStatus ? (
+                    <article className="flex flex-col space-y-4 text-lg m-8 md:m-16 items-center justify-items-center bg-black py-4 rounded-lg bg-opacity-30 mb-24">
+                        <p className="w-4/5 indent">The name is the brand - we are design and technical professionals, collaborating together to bring art to our disciplines, and craftsmanship to our projects. Our skills range from architecture to graphic arts, and from creative writing to web development. Commitment to art and craft is the cornerstone of our work - building a reliable foundation for the services we provide.</p>
+                        <p className="w-4/5 indent">We believe all work has intrinsic value. Some of the work on this web site is for business purposes, and some is a form of creative expression. All is intended as contribution to the common good as we complement each other's skills and strengths.</p>
+                        <p className="w-4/5 indent">If you have a project you would like us to collaborate with you on as a client - or if you want to bring your skills to the group - please reach out to one of our maker-partners. You can search for our offerings and project examples either by "Design Disciplines" or by "Collaboration Partners".</p>
+                    </article>
+                ) : (
+                    null
+                )
+            }
 
             <span className="absolute bottom-0 right-0 m-2 text-sm">Photo by <a href="https://unsplash.com/@mana5280?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">mana5280</a> on <a href="https://unsplash.com/@mana5280?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText" target="_blank" rel="noopener noreferrer">Unsplash</a></span>
 
