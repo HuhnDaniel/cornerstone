ad802188930591cbab922b51ef11335ff29b628e
HuhnDaniel <danielphuhn@gmail.com>
2020-12-25 17:16:20 -0600

    contact page background and styling

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -5,20 +5,22 @@ import Footer from '../components/Footer';
 
 function Contact({ menuStatus, menuToggle }) {
 	return (
-		<main onClick={menuToggle} className="fixed top-0 left-0 h-full w-full text-2xl">
+		<main onClick={menuToggle} className="absolute top-0 left-0 bg-contact bg-fixed min-h-full min-w-full bg-cover text-white text-2xl">
             <Header menuStatus={menuStatus} />
 
-            <article className="m-8">
-                Cornerstone Art &amp; Craft Collaborative LLC<br />
-                OverlandPark, KS<br />
-                Founding Principal - Barry Huhn<br />
-                <a className="text-blue-600" href="mailto:cornerstone.art.craft@gmail.com">cornerstone.art.craft@gmail.com</a>
-            </article>
+			<section className="flex flex-col space-y-4 text-lg m-8 md:m-16 items-start justify-items-center bg-black p-4 rounded-lg bg-opacity-30 mb-24">
+				<article>
+					Cornerstone Art &amp; Craft Collaborative LLC<br />
+					OverlandPark, KS<br />
+					Founding Principal - Barry Huhn<br />
+					<a className="text-blue-600" href="mailto:cornerstone.art.craft@gmail.com">cornerstone.art.craft@gmail.com</a>
+				</article>
 
-            <article className="m-8">
-                Technical Support<br />
-                Submit an issue: <a className="text-blue-600" href="https://github.com/HuhnDaniel/cornerstone/issues/new" target="_blank"  rel="noopener noreferrer">Link</a>
-            </article>
+				<article>
+					Technical Support<br />
+					Submit an issue: <a className="text-blue-600" href="https://github.com/HuhnDaniel/cornerstone/issues/new" target="_blank"  rel="noopener noreferrer">Link</a>
+				</article>
+			</section>
 
 			<Footer />
 		</main>
