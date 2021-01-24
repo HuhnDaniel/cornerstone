41c71457bd37ccac581eb7bfdb56a3caabcd84da
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-01-24 16:30:12 -0600

    api route for get project by id, import into project page

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
+import API from '../utils/API';
+
 function Project({ menuStatus, menuToggle }) {
+    const { projId } = useParams();
+
+    const [project, setProject] = useState({});
+
+    useEffect(() => {
+        getProject();
+    }, []);
+
+    async function getProject() {
+        const { data } = await API.getProjectById(projId);
+
+        setProject(data[0]);
+    }
+
     return (
         <main onClick={menuToggle} className="absolute min-h-full min-w-full">
             <Header menuStatus={menuStatus} />
+
+            {
+                project ? (
+                    <h1>{project.name}</h1>
+                ) : (
+                    null
+                )
+            }
 
 			<Footer />
         </main>
