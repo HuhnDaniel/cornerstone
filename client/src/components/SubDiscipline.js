85851286bcf84fdbab38282c39967d74eee5f920
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-01-25 10:52:53 -0600

    start transitioning images to img elements, move to public folder

--- client/src/components/SubDiscipline.js
+++ client/src/components/SubDiscipline.js
@@ -10,12 +10,11 @@ function SubDiscipline({ subDiscipline }) {
                 {
                     subDiscipline.Projects.map((project, i) => {
                         return (
-                            <Link to={`/project/${project.id}`} key={i}>
-                                <article className={`bg-${project.image}Sq bg-cover rounded-md cursor-pointer inline-block m-4 h-40 w-40 hover:shadow-md transition transition-transform duration-200 transform hover:scale-105`}>
-                                    <div className="bg-black p-4 rounded-t-md bg-opacity-30">
-                                        <h3 className="text-md text-white overflow-hidden truncate">{project.name}</h3>
-                                    </div>
-                                </article>
+                            <Link to={`/project/${project.id}`} className="relative rounded-md cursor-pointer inline-block m-4 h-40 w-40 hover:shadow-md transition transition-transform duration-200 transform hover:scale-105" key={i}>
+                                <img src={`/images/${project.image}-sq.jpg`} className={``} />
+                                <div className="absolute top-0 left-0 bg-black p-4 rounded-t-md bg-opacity-30 w-40">
+                                    <h3 className="relative text-md text-white overflow-hidden truncate">{project.name}</h3>
+                                </div>
                             </Link>
                         );
                     })
