2052afb388cbfc297d44116d1ae72cf85ce74fa3
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-02-03 20:50:55 -0600

    overlay closes when chosing projects on project page

--- client/src/components/SubDiscipline.js
+++ client/src/components/SubDiscipline.js
@@ -10,9 +10,9 @@ function SubDiscipline({ subDiscipline }) {
                     subDiscipline.Projects.map((project, i) => {
                         return (
                             <Link to={`/project/${project.id}`} className="relative rounded-md cursor-pointer inline-block m-4 h-40 w-40 hover:shadow-md transition transition-transform duration-200 transform hover:scale-105" key={i}>
-                                <img src={`/images/${project.image}-sq.jpg`} alt={project.name} />
+                                <img className="rounded-md" src={`/images/${project.image}-sq.jpg`} alt={project.name} />
                                 <div className="absolute top-0 left-0 bg-black p-4 rounded-t-md bg-opacity-30 w-40">
-                                    <h3 className="relative text-md text-white overflow-hidden truncate">{project.name}</h3>
+                                    <h3 className="relative text-md text-white leading-4 whitespace-normal">{project.name}</h3>
                                 </div>
                             </Link>
                         );
