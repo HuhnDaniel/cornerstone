502505600acc37921890ec92fc5e149d5f699c1f
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-02-17 19:34:32 -0600

    try routing through dirname

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('/client/build'));
+    app.use('/', express.static(path.join(__dirname, '/client/build')));
     app.get("/favicon.ico", (req, res) => {
         res.sendFile(path.resolve(__dirname, "/favicon.ico"));
     });
