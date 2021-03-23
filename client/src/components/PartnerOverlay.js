aebc9cd680f8b13580fddcab8c8f0de8925e9019
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-03-22 19:40:03 -0500

    partners overlay fixed when scrolling

--- client/src/components/PartnerOverlay.js
+++ client/src/components/PartnerOverlay.js
@@ -18,8 +18,7 @@ class PartnerOverlay extends Component {
             return data[0];
         }
 
-
-        if (this.props !== prevProps) {
+        if (this.props !== prevProps && this.state.id != this.props.currentPartner) {
             const partner = await getPartner(this.props.currentPartner);
 
             if (Boolean(partner)) {
@@ -33,16 +32,22 @@ class PartnerOverlay extends Component {
     render() {
         if (this.props.overlayVisibility) {
             return (
-                <section data-id="margin" className={`${this.props.overlayPositioning} flex items-center justify-center top-0 left-0 h-full w-full pb-24`} onClick={this.props.closeOverlay}>
-                    <div className="flex flex-col md:flex-row h-11/12 w-full sm:rounded sm:w-11/12 bg-gray-400 opacity-100">
-                        <div className="flex flex-col flex-1 px-4 pb-4 md:pt-4 border-b-2 md:border-0 overflow-y-auto">
-                            <h2 data-id="close" className="self-end text-xl cursor-pointer p-4 md:hidden">⨯</h2>
+                <section data-id="margin" className={`absolute flex items-center justify-center top-0 left-0 pb-24 h-full w-full`} onClick={this.props.closeOverlay}>
+                    <div className={`${this.props.overlayPositioning} flex flex-col md:flex-row p-4 h-3/4 w-full sm:rounded sm:w-11/12 bg-gray-400 opacity-100 overflow-y-auto md:overflow-y-hidden`}>
+                        <div className="hidden md:flex flex-col flex-initial md:flex-1 p-4 overflow-y-auto">
                             <h1 className="text-2xl mb-4">{this.state.name}</h1>
 							<img className="rounded-md h-60 w-60 mx-auto mb-4" src={this.state.profilePic ? `/images/${this.state.profilePic}.jpg` : "/images/default-user.svg"} alt={this.state.name} />
                             <p className="text-lg mb-4">{this.state.about}</p>
                             <a href={`mailto:${this.state.email}`} className="text-lg">{this.state.email}</a>
                         </div>
-                        <div className="flex flex-col flex-1 md:flex-2 p-4 md:pt-0 overflow-y-auto overflow-x-hidden">
+
+                        <h2 data-id="close" className="absolute self-end text-xl cursor-pointer px-4 md:hidden">⨯</h2>
+                        <h1 className="text-2xl mb-4 md:hidden">{this.state.name}</h1>
+						<img className="rounded-md h-60 w-60 mx-auto mb-4 md:hidden" src={this.state.profilePic ? `/images/${this.state.profilePic}.jpg` : "/images/default-user.svg"} alt={this.state.name} />
+                        <p className="text-lg mb-4 md:hidden">{this.state.about}</p>
+                        <a href={`mailto:${this.state.email}`} className="text-lg md:hidden">{this.state.email}</a>
+
+                        <div className="flex flex-col flex-2 p-4 md:pt-0 md:overflow-y-auto">
                             <h2 data-id="close" className="self-end text-xl cursor-pointer p-4 hidden md:block">⨯</h2>
                             <div className="relative grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 justify-items-center px-8 pb-4">
                                 {
@@ -52,7 +57,9 @@ class PartnerOverlay extends Component {
                                                 <ProjectCard project={project} key={i} />
                                             )
                                         })
-                                    ) : ( null )
+                                    ) : (
+                                        null
+                                    )
                                 }
                             </div>
                         </div>
