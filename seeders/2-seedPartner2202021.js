24a8074f789e27c6602460e3b2bab458a0a61ae2
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-04-07 16:42:13 -0500

    add logos, styling

--- seeders/2-seedPartner2202021.js
+++ seeders/2-seedPartner2202021.js
@@ -5,7 +5,7 @@ module.exports = {
         await queryInterface.bulkInsert('Partners', [
             {
                 name: 'Barry Huhn',
-                email: 'Barry.huhn@gmail.com',
+                email: 'cornersone.art.craft@gmail.com',
                 about: 'Barry is a collaborative architect and team leader, helping clients bring their vision into built reality. Mr. Huhn has extensive experience in architecture and planning, having led multi-disciplinary teams to successfully complete numerous public assembly, sports, and commercial architecture projects. See examples of experience in the project thumbnails on this page. Bringing a balance of creative thinking with technical expertise, Barry can work with your team from Master Planning through Construction Administration to provide services and strategies to accomplish project success. Mr. Huhn founded Cornerstone Art & Craft Collaborative to create a platform for various creative designers and clients to work together. If you have a project you would like help on, or would like to join our collaboration team, please contact us!',
                 profilePic: 'barry-huhn'
             },
