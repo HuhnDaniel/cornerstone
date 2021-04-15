ee8b4bfc1d481bd816f19fc40ed57ce013244720
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2021-04-15 15:26:06 -0500

    add react-https-redirect

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import HttpsRedirect from 'react-https-redirect';
+
 import './assets/tailwind/main.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <HttpsRedirect>
+        <App />
+    </HttpsRedirect>,
+    document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
