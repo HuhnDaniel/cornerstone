3c3284d8658ea9ab443f197cdc03a078fbfcda98
Daniel Moody-Huhn <60518682+HuhnDaniel@users.noreply.github.com>
2022-01-29 21:36:59 -0600

    Admin page navigation styling

---
+++ client/src/components/admin_components/OptionsNav.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function OptionsNav() {
+    return (
+        <section className="m-8 w-5/6 sm:w-11/12 md:w-1/3 lg:w-1/4">
+            <article className="mb-4">
+                <h2 className="text-2xl bg-blue-300 p-2">Discipline Topics</h2>
+                <ul className="text-lg">
+                    <li className="p-2">Disciplines</li>
+                    <li className="p-2">Sub-Disciplines</li>
+                </ul>
+            </article>
+            <article>
+                <h2 className="text-2xl bg-blue-300 p-2">Partner Topics</h2>
+                <ul className="text-lg">
+                    <li className="p-2">Partners</li>
+                    <li className="p-2">Projects</li>
+                </ul>
+            </article>
+        </section>
+    );
+}
+
+export default OptionsNav;
--- client/src/pages/admin_pages/AdminHome.js
+++ client/src/pages/admin_pages/AdminHome.js
@@ -1,10 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AdminHeader from '../../components/admin_components/AdminHeader';
+import OptionsNav from '../../components/admin_components/OptionsNav';
 
 function AdminHome() {
     return (
         <main>
             <AdminHeader />
+            <OptionsNav />
         </main>
     );
 }
